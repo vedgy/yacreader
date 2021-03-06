c6098382feaf61c9adb6f629e95c7fabe07fa992
Igor Kushnir <igorkuo@gmail.com>
2021-03-06 18:39:01 +0200

    Extract Viewer::(vertical|horizontal)ScrollStep()

--- YACReader/viewer.h
+++ YACReader/viewer.h
@@ -165,6 +165,9 @@ private:
 
     void moveAction(const QKeySequence &key);
 
+    int verticalScrollStep() const;
+    int horizontalScrollStep() const;
+
     //!ZigzagScroll
     enum scrollDirection { UP,
                            DOWN,
